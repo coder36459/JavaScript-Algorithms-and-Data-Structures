fbdc80f08bac4eea44c207cac3a9339e178cf260
Maciej Browarski <140039573+coder36459@users.noreply.github.com>
2024-10-25 20:55:22 +0200

    Update script.js

--- Cash-Register/script.js
+++ Cash-Register/script.js
@@ -10,17 +10,21 @@ let cid = [
   ['TWENTY', 60],
   ['ONE HUNDRED', 100],
 ];
-const cash = document.getElementById("cash");
+const displayCash = document.getElementById("cash");
 const changeDue = document.getElementById("change-due");
 const purchaseBtn = document.getElementById("purchase-btn");
-const testBtn = document.getElementById("test");
-const resetBtn = document.getElementById("reset");
-const displayPrice = document.getElementById("price");
+const displayPrice = document.getElementById("display-price");
+const displayDue = document.getElementById("display-due");
+const testBtn = document.getElementById("test-btn");
+const resetBtn = document.getElementById("reset-btn");
+const displayCID = document.getElementById("display-cid");
 const displayTotalCashInDrawer = document.getElementById("total-cash-in-drawer");
+const displaySolution = document.getElementById("solution");
 const tip = document.getElementById("tip");
 const cashValue = () => {
-	return cash.value * 1;
+	return displayCash.value * 1;
 }
+//displayCash.value = 20;
 const amount = [10000, 2000, 1000, 500, 100, 25, 10, 5, 1];
 const roundToTwoDecimalPlaces = num => {
 	return Math.round((num + Number.EPSILON) * 100) / 100;
@@ -44,10 +48,10 @@ purchaseBtn.addEventListener("click", () => {
 	let chd = [];
 	let d = 0, d1 = 0, m = 0, n = 0;
 	if (cashValue() < price) {
-		alert("Customer does not have enough money to purchase the item");
+		alert("Customer does not have enough money to purchase the item.");
 	}
 	else if (cashValue() === price) {
-		changeDue.innerText = "No change due - customer paid with exact cash";
+		changeDue.innerText = "No change due - customer paid with exact cash.";
 	}
 	else {
 		let due = (cashValue() - price) * 100;
@@ -86,30 +90,52 @@ purchaseBtn.addEventListener("click", () => {
 			changeDue.innerText = "Status: OPEN\n" + status(chd);
 		}
 		else if (price < cashValue() && cidBefore === (cashValue() - price)) {
-			changeDue.innerText = "Status: CLOSED" + "\n" + status(chd);
+			changeDue.innerText = "Status: CLOSED\n" + status(chd);
 		}
 		else {
 			changeDue.innerText = "Status: INSUFFICIENT_FUNDS";
 		}
 	}
+	displayPrice.value = "$" + price;
+	if (cashValue() !== "" && cashValue() >= price) {
+		displayDue.value = "$" + roundToTwoDecimalPlaces(cashValue() - price);
+	}
+	else {
+		displayDue.value = "$" + 0;
+	}
+	displaySolution.innerText = "Below you will find more details.\n\n";
+	displaySolution.style.display = "block";
 });
+displayDue.value = "$" + 0;
 testBtn.innerText = "Test 1";
 resetBtn.innerText = "Reset";
-cash.value = 20;
-displayPrice.value = price;
-changeDue.innerText = "Status: ";
-displayTotalCashInDrawer.innerText = "Total cash in drawer:\n" + status(cid);
-cash.addEventListener("mouseover", () => {
-	tip.style.visibility = "visible";
-	tip.style.left = "50%";
-	tip.style.top = "0px";
-	tip.style.transform = "translateX(-50%)";
-	tip.style.backgroundColor = "rgba(250,216,250.7)";
-	tip.innerText = "The amount of cash provided by the customer for the item.";
-});
-cash.addEventListener("mouseout", () => {
-	tip.style.visibility = "hidden";
-});
+changeDue.innerText = "Status: READY";
+displayCID.innerText = "Total cash in drawer: $" + sumArray(cid);
+displayTotalCashInDrawer.innerText = status(cid);
+const tooltip = (id, text) => {
+	id.addEventListener("mouseover", () => {
+		tip.style.visibility = "visible";
+		tip.style.left = "50%";
+		tip.style.top = "0px";
+		tip.style.transform = "translateX(-50%)";
+		tip.style.backgroundColor = "rgba(250,216,250.7)";
+		tip.innerText = text;
+	});
+	id.addEventListener("mouseout", () => {
+		tip.style.visibility = "hidden";
+	});
+}
+tooltip(displayCash, "The amount of cash provided by the customer for the item.");
+tooltip(displayPrice, "The price of the item.");
+tooltip(displayDue, "Refund of change to the customer based on the price of the item.");
+tooltip(displayTotalCashInDrawer, "The amount of cash in the cash drawer.");
 resetBtn.addEventListener("click", () => {
-	cash.value = "";
+	displaySolution.innerText = "";
+	displaySolution.style.display = "none";
+	changeDue.innerText = "Status: READY";
+	displayCash.value = "";
+	displayDue.value = "$" + 0;
+});
+testBtn.addEventListener("click", () => {
+	testBtn.innerText = "Test 2";
 });
