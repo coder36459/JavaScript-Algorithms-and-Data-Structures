1d7650505eefd58efecf8cb578684df30f7e6fe1
Maciej Browarski <140039573+coder36459@users.noreply.github.com>
2024-10-28 21:32:47 +0100

    Update script.js

--- Roman-Numeral-Converter/script.js
+++ Roman-Numeral-Converter/script.js
@@ -1,53 +1,71 @@
+document.body.innerHTML = `<main><header><h1>Roman Numeral Converter</h1></header><section><input id="number" type="text"><button id="convert-btn"></button><button id="reset-btn"></button><div id="output"></div></section></main>`;
 const number = document.getElementById("number");
 const convertBtn = document.getElementById("convert-btn");
 const output = document.getElementById("output");
+const resetBtn = document.getElementById("reset-btn");
 const numerals = [[1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1], ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]];
-let count = 0;
-convertBtn.innerText = "Convert";
+const arabic = () => {
+	return number.value * 1;
+}
+const reset = () => {
+	convertBtn.style.display = "inline-block";
+	convertBtn.innerText = "Convert";
+	resetBtn.style.display = "none";
+	number.value = "";
+	output.innerText = "";
+	output.style.display = "none";
+}
+const create = () => {
+	convertBtn.style.display = "none";
+	resetBtn.style.display = "inline-block";
+	resetBtn.innerText = "Reset";
+}
+reset();
 convertBtn.addEventListener("click", () => {
-	const arabic = () => {return number.value * 1;}
-	let roman = "";
-	if (convertBtn.innerText === "Convert") {
-		convertBtn.innerText = "Reset";
-		let d = 0;
-		count = parseInt(arabic());
-		if (!arabic()) {
-			output.innerText = "Please enter a valid number";
-			output.style.display = "block";
-		}
-		else if (arabic() <= 0) {
-			output.innerText = "Please enter a number greater than or equal to 1";
-			output.style.display = "block";
-		}
-		else if (arabic() >= 4000) {
-			output.innerText = "Please enter a number less than or equal to 3999";
-			output.style.display = "block";
-		}
-		else {
+	create();
+	if (!arabic()) {
+		output.innerText = "Please enter a valid number";
+		output.style.display = "block";
+	}
+	else if (arabic() <= 0) {
+		output.innerText = "Please enter a number greater than or equal to 1";
+		output.style.display = "block";
+	}
+	else if (arabic() >= 4000) {
+		output.innerText = "Please enter a number less than or equal to 3999";
+		output.style.display = "block";
+	}
+	else {
+		const convertToRoman = (num) => {
+			let count = num, roman = "";
 			for (const x in numerals[0]) {
-				if (count % numerals[0][x] != count) {
-					d = Math.floor(count / numerals[0][x]);
-					if (d > 1) {
-						while (d > 0) {
-							count -= numerals[0][x];
-							roman += numerals[1][x];
-							d--;
-						}
+				let isItPossible = count % numerals[0][x];
+				let division = Math.floor(count / numerals[0][x]);
+				const addToString = () => {
+					count -= numerals[0][x];
+					roman += numerals[1][x];
+				}
+				if (count !== isItPossible) {
+					if (count === numerals[0][x]) {
+						addToString();
 					}
 					else {
-						count -= numerals[0][x];
-						roman += numerals[1][x];
+						if (division >= 2) {
+							count -= division * numerals[0][x];
+							roman += numerals[1][x].repeat(division);
+						}
+						else {
+							addToString();
+						}
 					}
 				}
 			}
 			return output.innerText = roman, output.style.display = "block";
-		}	
+		}
+		convertToRoman(arabic());
 	}
-	else {
-		convertBtn.innerText = "Convert";
-		number.value = "";
-		output.innerText = "";
-		output.style.display = "none";
-		roman = "";
-	}
+});
+resetBtn.addEventListener("click", () => {
+	reset();
+	roman = "";
 });
