d8f6d853bf1c40d4454784968801ebdf736331e7
Maciej Browarski <140039573+coder36459@users.noreply.github.com>
2024-11-03 20:08:35 +0100

    Update script.js

--- Pokémon-Search-App/script.js
+++ Pokémon-Search-App/script.js
@@ -12,6 +12,7 @@ const defense = document.getElementById("defense");
 const specialAttack = document.getElementById("special-attack");
 const specialDefense = document.getElementById("special-defense");
 const speed = document.getElementById("speed");
+const spritesIMG = document.getElementById("sprites-img");
 searchBtn.addEventListener("click", () => {
 	let item = (searchInput.value).toLowerCase();
 	//console.log(item)
@@ -27,7 +28,37 @@ searchBtn.addEventListener("click", () => {
 			}
 			else {
 				alert("Pokémon is found");
-				console.log(i);
+				const resourcesURLNameOrID = resourcesURL + "/" + i;
+				fetch(resourcesURLNameOrID)
+				.then((res) => res.json())
+				.then((data) => {
+					pokemonName.innerText = data["name"];
+					pokemonID.innerText = data["id"];
+					weight.innerText = data["weight"];
+					height.innerText = data["height"];
+					types.innerText = (data["types"][0]["type"]["name"]).toUpperCase();
+					hp.innerText = data["stats"][0]["base_stat"];
+					attack.innerText = data["stats"][1]["base_stat"];
+					defense.innerText = data["stats"][2]["base_stat"];
+					specialAttack.innerText = data["stats"][3]["base_stat"];
+					specialDefense.innerText = data["stats"][4]["base_stat"];
+					speed.innerText = data["stats"][5]["base_stat"];
+					spritesIMG.innerHTML = `<img id="sprite" src="${data["sprites"]["front_default"]}">`;
+					console.log("pokemon-name", data["name"])
+					console.log("pokemon-id", data["id"])
+					console.log("weight", data["weight"])
+					console.log("height", data["height"])
+					console.log("hp", data["stats"][0]["base_stat"])
+					console.log("attack", data["stats"][1]["base_stat"])
+					console.log("defense", data["stats"][2]["base_stat"])
+					console.log("special-attack", data["stats"][3]["base_stat"])
+					console.log("special-defense", data["stats"][4]["base_stat"])
+					console.log("speed", data["stats"][5]["base_stat"])
+					console.log("types", data["types"][0]["type"]["name"])
+					console.log("sprites", data["sprites"]["front_default"])
+					})
+				.catch((err) => console.error(err));
+				console.log(resourcesURLNameOrID);
 			}
 		}
 		if (Number.isInteger(searchInput.value * 1)) {
